bec7c29fe9c8aef7bf83e3a6303f8e3d7dc01e1c
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-02-08 15:08:48 +0100

    verifications

--- EB1.js
+++ EB1.js
@@ -1,43 +1,42 @@
 
-
 //EB1 JS
 //recuperer les donnees d'un API de l'id =0010024W
-fetch("https://data.education.gouv.fr/api/records/1.0/search/?dataset=fr-en-dnma-par-uai-appareils&q=&facet=debutsemaine&facet=uai&refine.uai=0010024W").then((data)=>{
+fetch("https://data.education.gouv.fr/api/records/1.0/search/?dataset=fr-en-dnma-par-uai-appareils&q=&facet=debutsemaine&facet=0010024W").then((data)=>{
         return data.json();
     }).then((objectData)=>{
      //console.log(objectData['records']) ; 
      let tab = [];
      const word="visites";    
-      for (let i = 0; i <=10; i++) { 
-               
+     for (let i = 0; i <=1; i++) { 
                let Object1=objectData.records[i].fields;
-               //console.log(Object1);
+               console.log(Object1);
                var s=0;
-        for(const key of Object.keys(Object1)){//parcour  chaque  key de chaque objet pour tester si il conteinet un mot visites
+        for(var key of Object.keys(Object1)){//parcour  chaque  key de chaque objet pour tester si il conteinet un mot visites
               let position =key.indexOf(word);
-              if(position===0){//càd le key contient le mot visites
-              var v = parseInt(Object1[key]);//je vais convertir chaque value string en entier 
+              if(position===-1){//càd le key contient le mot "visites"
+              console.log(position);}}
+              let v = parseInt(Object1[key]);//je vais convertir chaque value string en entier 
               s=s+v;}//faire la somme des visites
               //console.log(typeof(v));:: pour verifier le type
-               //console.log(v)
+            console.log(s)
               
-           }   
-          console.log(s); 
+            
+         /* console.log(s); 
          
-       let info = new Object();
-       info.nbr_visite = s;
-       info.date = Object1.debutsemaine;
-      console.log(info);
+         let info = new Object();
+         info.nbr_visite = s;
+         info.date = Object1.debutsemaine;
+        console.log(info);
 
 
         // Tri les données selon le nombre de visites
         info.sort((a, b) => b.s - a.s);
 
-          // Prendre les 3 premières semaines avec le plus grand nombre de visites
+          // Prendre les 3 premières semaines avec le plus grand nombre de visites*/
        
 
        
-                }
+                
                  
             
     });
