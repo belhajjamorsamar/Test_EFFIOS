ac0b71a5ffe1d4e7d2616b9a4f1c0f290aabb73f
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-02-09 02:04:03 +0100

    verifier

--- EB1.js
+++ EB1.js
@@ -1,44 +1,30 @@
+const api_data="https://data.education.gouv.fr/api/records/1.0/search/?dataset=fr-en-dnma-par-uai-appareils&q=&facet=debutsemaine&facet=uai&refine.uai=0010024W";
 
-//EB1 JS
-//recuperer les donnees d'un API de l'id =0010024W
-fetch("https://data.education.gouv.fr/api/records/1.0/search/?dataset=fr-en-dnma-par-uai-appareils&q=&facet=debutsemaine&facet=0010024W").then((data)=>{
-        return data.json();
-    }).then((objectData)=>{
-     //console.log(objectData['records']) ; 
-     let tab = [];
-     const word="visites";    
-     for (let i = 0; i <=1; i++) { 
-               let Object1=objectData.records[i].fields;
-               console.log(Object1);
-               var s=0;
-        for(var key of Object.keys(Object1)){//parcour  chaque  key de chaque objet pour tester si il conteinet un mot visites
-              let position =key.indexOf(word);
-              if(position===-1){//càd le key contient le mot "visites"
-              console.log(position);}}
-              let v = parseInt(Object1[key]);//je vais convertir chaque value string en entier 
-              s=s+v;}//faire la somme des visites
-              //console.log(typeof(v));:: pour verifier le type
-            console.log(s)
-              
-            
-         /* console.log(s); 
-         
-         let info = new Object();
-         info.nbr_visite = s;
-         info.date = Object1.debutsemaine;
-        console.log(info);
+const getData =async ()=>{
+      const res =await fetch(api_data);
+      const data=await res.json();
+      return data
+}
 
+const displayWeek =async() =>{
+ const payload = await getData();
+let variable=payload.records;
+let dataDisplay=variable.map((object)=>{
 
-        // Tri les données selon le nombre de visites
-        info.sort((a, b) => b.s - a.s);
+ let s= function somme(){
+      
+      return s;
 
-          // Prendre les 3 premières semaines avec le plus grand nombre de visites*/
-       
+ };
 
-       
-                
-                 
-            
-    });
-                                          
-                             //console.log(objectData.records[i].fields[10])
+ 
+ let info = new Object();
+ info.nbr_visite = s;
+ info.semaine = object.fields.debutsemaine;
+console.log(info);
+
+ 
+
+});
+}
+displayWeek();
